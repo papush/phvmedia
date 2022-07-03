12f80cee614f547bd8586a42b3330011624060ea
papush <papush8@gmail.com>
2022-07-03 23:09:50 +0200

    asd

---
+++ js/mobnav.js
@@ -0,0 +1,8 @@
+function mobNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
